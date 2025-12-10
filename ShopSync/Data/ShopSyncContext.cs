dd879bd5f9cb83a98612835a9431e72e55329f54
Dhrumit Parmar <dhamuparmar105@gmail.com>
2025-12-11 00:57:13 +0530

    Reapply "Remove .vs cache folder"
    This reverts commit b8400e8de81d6c60ce68d13029b80694b5433814.

--- ShopSync/Data/ShopSyncContext.cs
+++ ShopSync/Data/ShopSyncContext.cs
@@ -9,7 +9,7 @@ namespace ShopSync.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
-        //public DbSet<Customer> Customers { get; set; }
+        public DbSet<Customer> Customers { get; set; }
         //public DbSet<Bill> Bills { get; set; }
         //public DbSet<BillItem> BillItems { get; set; }
         //public DbSet<User> Users { get; set; }
--- ShopSyncClient/src/App.jsx
+++ ShopSyncClient/src/App.jsx
@@ -5,6 +5,7 @@ import Layout from "./layouts/Layout";
 import DashboardPage from "./pages/DashboardPage";
 import CategoryPage from "./pages/CategoryPage";
 import ProductPage from "./pages/ProductPage";
+import CustomerPage from "./pages/CustomerPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<DashboardPage />} />
         <Route path="/categories" element={<CategoryPage />} />
         <Route path="/products" element={<ProductPage />} />
+        <Route path="/customers" element={<CustomerPage />} />
       </Routes>
     </Layout>
   );
